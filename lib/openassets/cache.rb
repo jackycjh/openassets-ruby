47dddeef04e27fac17d5199d1739bce2b8d9a1ac
Jacky C <jackycjh@users.noreply.github.com>
2018-06-14 15:20:04 +0800

    Fix autoloading error.

--- lib/openassets/cache.rb
+++ lib/openassets/cache.rb
@@ -6,6 +6,6 @@ module OpenAssets
     autoload :OutputCache, 'openassets/cache/output_cache'
     autoload :CacheProviderBase, 'openassets/cache/cache_provider_base'
     autoload :SqliteCacheProvider, 'openassets/cache/sqlite_cache_provider'
-    autoload :MysqlProviderBase, 'openassets/cache/mysql_cache_provider'
+    autoload :MysqlCacheProvider, 'openassets/cache/mysql_cache_provider'
   end
 end
